595e0afa512fdbb1cd369c63218a99b6ef916825
Tahseen Rahman <tahseen.rahman@saarthi.ai>
2019-07-12 10:21:38 +0530

    .

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import customer
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+#from django.contrib import messages
 
 @login_required
 def index(request):
@@ -21,18 +21,26 @@ def checkin(request):
             phone = request.POST.get("phone")
             #date = request.POST.get("date")
             #time = request.POST.get("time")
-            c = customer(name=name, address=address, bed=bed, phone=phone, present=True)
-            c.save()
-            return HttpResponse("Customer Created")
+            if customer.objects.filter(bed=bed, present=True):
+                msg = "The bed is already occupied"
+            else:
+                c = customer(name=name, address=address, bed=bed, phone=phone, present=True)
+                c.save()
+                msg = "New Customer Checked In at bed:"+bed
         
         if t == "out":
             bed = request.POST.get("bed")
             c = customer.objects.filter(bed=bed, present=True).update(present=False)
             if c:
                 msg = "Customer with bed number:"+bed+" checked out"
-                return render(request, "home.html", {"msg":msg})
             else:
                 msg = "Customer doesn't exist"
-                return render(request, "home.html", {"msg":msg})
+                
+        context = {"msg":msg}
+        return render(request, "home.html", context)
     else:
         return render(request, "home.html")
+
+@login_required
+def search(request):
+    
